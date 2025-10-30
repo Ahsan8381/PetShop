18e4b695539e96034d8937459091fa4b95df5eb2
unknown <Ahsan8381>
2025-10-30 11:12:05 +0530

    server config

--- server.js
+++ server.js
@@ -8,13 +8,16 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
-
+//esmodulefix
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = path.__dirname(__fileName);
 //rest object
 const app = express();
 
